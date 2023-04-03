9ea151765556657d65c3271356700498555f9e0e
Rizvan555 <rizvan84@gmx.de>
2023-04-03 14:30:10 +0200

    part1 is finished

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,31 +6,50 @@ const searchNews = () => {
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
-      data.articles.forEach((article, index) => {
-        const sourceName = document.querySelector(`.source-name-${index}`);
-        const description = document.querySelector(`.description-${index}`);
-        const author = document.querySelector(`.author-${index}`);
-        const title = document.querySelector(`.title-${index}`);
-        const content = document.querySelector(`.content-${index}`);
+      const sourceName = (document.querySelector(
+        ".source-name"
+      ).textContent = `${data.articles[99].source.name}`);
 
-        const descImage = document.querySelector(`.descImage-${index}`);
-        const publishedDate = document.querySelector(`.publishedDate-${index}`);
+      const description = (document.querySelector(
+        ".description"
+      ).textContent = `${data.articles[99].description}`);
 
-        sourceName.textContent = `${article.source.name}`;
-        description.textContent = `${article.description}`;
-        descImage.src = `${article.urlToImage}`;
-        title.textContent = `${article.title}`;
-        content.textContent = `${article.content}`;
-        author.textContent = `${article.author}`;
-        publishedDate.textContent = `${article.publishedAt}`;
-      });
+      const author = (document.querySelector(
+        ".author"
+      ).textContent = `${data.articles[99].author}`);
+
+      const title = (document.querySelector(
+        ".title"
+      ).textContent = `${data.articles[99].title}`);
+      const title1 = (document.querySelector(
+        ".title1"
+      ).textContent = `${data.articles[3].title}`);
+      const title2 = (document.querySelector(
+        ".title2"
+      ).textContent = `${data.articles[1].title}`);
+      const title3 = (document.querySelector(
+        ".title3"
+      ).textContent = `${data.articles[2].title}`);
+
+      const content = (document.querySelector(
+        ".content"
+      ).textContent = `${data.articles[99].content}`);
+
+      const descImage = (document.querySelector(
+        ".descImage"
+      ).src = `${data.articles[99].urlToImage}`);
+      const descImage1 = (document.querySelector(
+        ".descImage1"
+      ).src = `${data.articles[3].urlToImage}`);
+      const descImage2 = (document.querySelector(
+        ".descImage2"
+      ).src = `${data.articles[1].urlToImage}`);
+      const descImage3 = (document.querySelector(
+        ".descImage3"
+      ).src = `${data.articles[2].urlToImage}`);
+
+      const publishedDate = (document.querySelector(
+        ".publishedDate"
+      ).textContent = `${data.articles[99].publishedAt}`);
     });
-
-  //   fetch(
-  //     `https://newsapi.org/v2/everything?q=apple&from=2023-04-02&to=2023-04-02&sortBy=popularity&apiKey=64b826567a8a457380cca1f368675e5c`
-  //   )
-  //     .then((response) => response.json())
-  //     .then((data) => {
-  //       console.log(data);
-  //     });
 };
